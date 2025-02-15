aed5ba8fe18d4225302d5a2c5b002911d2bc4c4e
WikiCoding <tiagomscastro@gmail.com>
2025-02-15 23:19:32 +0000

    Refactored Services

--- src/main/java/com/wikicoding/concurrencydemo/configs/AsyncConfig.java
+++ src/main/java/com/wikicoding/concurrencydemo/configs/AsyncConfig.java
@@ -1,4 +1,16 @@
 package com.wikicoding.concurrencydemo.configs;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Component
 public class AsyncConfig {
+
+    @Bean
+    public ExecutorService execService() {
+        return Executors.newFixedThreadPool(32);
+    }
 }
--- src/main/java/com/wikicoding/concurrencydemo/services/ServiceAsync.java
+++ src/main/java/com/wikicoding/concurrencydemo/services/ServiceAsync.java
@@ -1,9 +1,12 @@
 package com.wikicoding.concurrencydemo.services;
 
 import com.wikicoding.concurrencydemo.abstractions.MoviesService;
+import com.wikicoding.concurrencydemo.configs.AsyncConfig;
 import com.wikicoding.concurrencydemo.models.Movie;
 import com.wikicoding.concurrencydemo.repos.MoviesRepository;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.CompletableFuture;
@@ -14,43 +17,38 @@ import java.util.concurrent.Executors;
 @AllArgsConstructor
 public class ServiceAsync implements MoviesService {
     private final MoviesRepository moviesRepository;
+    private final ExecutorService execService;
+    private final Logger logger = LoggerFactory.getLogger(ServiceAsync.class);
 
     public CompletableFuture<Iterable<Movie>> getAllMovies() {
         CompletableFuture<Iterable<Movie>> future = new CompletableFuture<>();
 
-        try (ExecutorService executorService = Executors.newFixedThreadPool(32)) {
-            executorService.submit(() -> {
-                long start = System.currentTimeMillis();
-                Iterable<Movie> movies = moviesRepository.findAll();
-                long end = System.currentTimeMillis();
+        execService.submit(() -> {
+            long start = System.currentTimeMillis();
+            Iterable<Movie> movies = moviesRepository.findAll();
+            long end = System.currentTimeMillis();
 
-                System.out.println("Query took: " + (end - start) + " ms.");
+            logger.info("Running on Thread {}", Thread.currentThread().getName());
+            logger.info("Query took: {} ms.", end - start);
 
-                future.complete(movies);
-            });
+            future.complete(movies);
+        });
 
-            return future;
-        } catch (RuntimeException e) {
-            System.out.println(e.getMessage());
-            return future;
-        }
+        return future;
     }
 
     public CompletableFuture<Movie> saveMovie(Movie movie) {
         CompletableFuture<Movie> future = new CompletableFuture<>();
 
-        try (ExecutorService executorService = Executors.newFixedThreadPool(32)) {
-            executorService.submit(() -> {
-                long start = System.currentTimeMillis();
-                moviesRepository.save(movie);
-                long end = System.currentTimeMillis();
-                System.out.println("Movie saved took: " + (end - start) + " ms.");
-                future.complete(movie);
-            });
-            return future;
-        } catch (RuntimeException e) {
-            System.out.println(e.getMessage());
-            return future;
-        }
+        execService.submit(() -> {
+            long start = System.currentTimeMillis();
+            moviesRepository.save(movie);
+            long end = System.currentTimeMillis();
+            logger.info("Running on Thread {}", Thread.currentThread().getName());
+            logger.info("Movie saved took: {} ms.", end - start);
+            future.complete(movie);
+        });
+
+        return future;
     }
 }
